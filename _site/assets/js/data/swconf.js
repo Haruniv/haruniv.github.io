360ca407b193ccffaac5a8c5271f61be8f42efd4
haruniv <harunivv@gmail.com>
2024-08-14 17:53:16 +0900

    blog code5

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1723618028',resources: [
+    cacheName: 'chirpy-1723622928',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
